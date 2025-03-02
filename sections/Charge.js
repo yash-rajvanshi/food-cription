0819c7261693c77ac191493f5c5a4d6552b5a756
yash-rajvanshi <yashrajvanshi@Beast-2.local>
2025-03-02 20:18:18 +0530

    made why

---
+++ sections/Charge.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Charge = () => {
+  return (
+    <div className="max-w-3xl mx-auto p-6 text-center space-y-6 mb-16">
+      <h1 className="text-3xl md:text-5xl font-bold leading-tight">
+         <span className='text-orange-500'>Take Charge</span> of Your Health Today!
+      </h1>
+      <p className="text-lg md:text-3xl text-gray-700 font-zain">
+        Say goodbye to unhealthy eating habits and hello to a healthier, happier you. 
+        Subscribe to FoodCription now and start eating according to what your body truly needs!
+      </p>
+      <h2 className="text-2xl md:text-4xl font-semibold">
+        <span className='text-[#5AB008]'>FoodCription</span> – Because Eating Right Should Be Easy.
+      </h2>
+    </div>
+  );
+};
+
+export default Charge;
+
