1d2415432780990c6d4a434e613ef9bbe31f836b
Bloomer004 <ishaguptavnsv@gmail.com>
2025-03-02 17:07:23 +0530

    Because section completed with responsiveness.

--- app/homePage/page.js
+++ app/homePage/page.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from '@/sections/Footer'
 import LandingPage from '@/sections/LandingPage'
 import Welcome from '@/sections/Welcome'
+import Why from '@/sections/Why'
 const HomePage = () => {
   return (
     <div>
       <LandingPage/>
       <Welcome/>
+      <Why/>
       <Footer/>
     </div>
   )
---
+++ sections/Why.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Why1 from "@/assets/images/Why3.jpg"
+
+const How = () => {
+  return (
+    <div>
+      <div className='w-full h-[100vh] relative mb-16 ' style={{ backgroundImage: ` url(${Why1.src})`, backgroundSize: "contain",backgroundPositionX:"right", backgroundRepeat: "no-repeat" }}>
+        <div className='absolute right-[15rem] top-[4rem]'>Helooo World</div>
+        <div className='absolute right-[20rem] top-[15rem]'>Helooo World</div>
+        <div className='absolute right-[20rem] top-[28rem]'>Helooo World</div>
+        <div className='absolute right-[15rem] top-[40rem]'>Helooo World</div>
+      </div>
+    </div>
+  )
+}
+
+export default How
